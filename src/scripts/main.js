c8da0e9e7927f5ffb288c26aba29af27d403d4fc
Denis Miroshnikov <genuine9x@gmail.com>
2022-10-18 13:04:41 +0300

    Настроил билд

---
+++ src/scripts/main.js
@@ -0,0 +1,36 @@
+import Swiper from "swiper/swiper-bundle";
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox.css";
+import "swiper/swiper-bundle.css";
+
+const centerSlider = new Swiper(".center-slider__wrapper", {
+  centeredSlides: true,
+  loop: true,
+  effect: "coverflow",
+  autoHeight: true,
+  slidesPerView: 3,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 20,
+    modifier: 0,
+    slideShadows: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets'
+  },
+  navigation: {
+    nextEl: '.center-slider__button-next',
+    prevEl: '.center-slider__button-prev',
+  },
+});
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+  Thumbs: {
+    Carousel: {
+      fill: false,
+      center: true,
+    },
+  },
+});
