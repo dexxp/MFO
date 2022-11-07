2e9b7cf6ae36abcca56e28063c4e5eb7b4ecbf88
Darya Gurina <mamadommaa@gmail.com>
2022-11-07 16:32:51 +0300

    add add-services block

--- dist/main.d8ebb8d6.js
+++ dist/main.d8ebb8d6.js
@@ -12066,7 +12066,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60565" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55748" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
