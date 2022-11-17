683f415e725127459a47fd81e22701793d089ef7
Denis Miroshnikov <genuine9x@gmail.com>
2022-11-17 23:13:58 +0300

    presonal cabinet, webinar pages

--- src/scripts/cert.js
+++ src/scripts/cert.js
@@ -6,8 +6,9 @@ import "swiper/swiper-bundle.css";
 window.addEventListener("DOMContentLoaded", () => {
   const certSlider = new Swiper(".cert-slider__wrapper", {
     centeredSlides: true,
-    slidesPerView: 5,
+    slidesPerView: "auto",
     loop: true,
+    spaceBetween: 40,
     pagination: {
       el: ".cert-slider__pagination",
       clickable: true,
@@ -16,17 +17,5 @@ window.addEventListener("DOMContentLoaded", () => {
       nextEl: ".cert-slider__button-next",
       prevEl: ".cert-slider__button-prev",
     },
-  });
-
-  const activeSlide = document.querySelector(".swiper-slide-active");
-  activeSlide.previousElementSibling.previousElementSibling.style.opacity = 0.5;
-  activeSlide.nextElementSibling.nextElementSibling.style.opacity = 0.5;
-
-  certSlider.on("slideChangeTransitionEnd", (e) => {
-    const slides = [...e.slides];
-    const activeSlide = slides.find(el => el.classList.contains("swiper-slide-active"));
-    slides.forEach(el => el.style.opacity = 1);
-    activeSlide.previousElementSibling.previousElementSibling.style.opacity = 0.5;
-    activeSlide.nextElementSibling.nextElementSibling.style.opacity = 0.5;
   });
 });
